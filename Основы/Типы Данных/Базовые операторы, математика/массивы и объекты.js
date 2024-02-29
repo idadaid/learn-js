d556db087d2ab9dbe832c46dbbbcc66286329e3b
Adel Garaeva <psinasutulaya228000@gmail.com>
2024-02-29 20:08:18 +0300

    массивы и объекты

---
+++ Основы/Типы Данных/Базовые операторы, математика/массивы и объекты.js
@@ -0,0 +1,12 @@
+const user = {
+userName: 'Adele',
+age: 22,
+isAdmin: true,
+email: 'psinasutulaya228000@gmail.com',
+'user-address': {
+    city: Moscow,
+},
+skills: ['html', 'css', 'js']
+};
+
+console.log(user);
